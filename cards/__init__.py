d034e94f1cd355f1625862b46a16ebc2490cacf7
curtis.stein <curtis.stein>
2017-07-18 12:06:50 -0500

    Edited Card and Dice code.  Added Readme and setup config files

--- cards/__init__.py
+++ cards/__init__.py
@@ -11,7 +11,7 @@ class Card(object):
                  'A': 14
                  }
         self.rank = rank
-        self.suit = suit
+        self.suit = suit.upper()
         self.value = int(rank) if rank.isdigit() else ranks[rank]
 
     def __repr__(self):
@@ -24,6 +24,18 @@ class Deck(object):
         self.ranks = '23456789TJQKA'
         self.order = list(Card(card[0], card[1]) for card in product(self.ranks, self.suits))
         self.order = self.order*num
+        self.index = 0
 
     def shuffle(self):
         random.shuffle(self.order)
+        self.index = 0
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.index >= len(self.order):
+            raise StopIteration
+        card = self.order[self.index]
+        self.index += 1
+        return card
