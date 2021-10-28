2b56e9da3527e19b582fa6f1eb3b29f3f04f98d7
SubZeb <cjstein9@gmail.com>
2021-10-28 11:43:15 -0500

    Create dice.py

---
+++ dice/dice.py
@@ -0,0 +1,16 @@
+from itertools import product
+from random import randint
+
+
+class Dice(object):
+    def __init__(self, num=1, sides=6):
+        self.num = num
+        self.sides = sides
+        self.values = []
+        for i in range(self.num):
+            self.values.append(randint(1,self.sides))
+        self.roll()
+
+    def roll(self):
+        for i in range(self.num):
+            self.values[i] = randint(1, self.sides)
