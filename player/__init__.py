9ee3686ab3b3684c15029429b7bd24c2d1e77c78
SubZeb <cjstein9@gmail.com>
2021-10-28 11:44:17 -0500

    Update __init__.py
    moving to separate file

--- player/__init__.py
+++ player/__init__.py
@@ -1,10 +1,1 @@
-class CardPlayer(object):
-    def __init__(self, name=None):
-        self.hand = []
-        self.name = name
 
-    def new_card(self, card):
-        self.hand.append(card)
-
-    def new_hand(self):
-        self.hand = []
